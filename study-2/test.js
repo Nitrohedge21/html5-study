da3e40831a23fc096b1290fef5783cbf236e125c
Ersan <ersanefe21@gmail.com>
2026-03-18 16:37:16 +0300

    stuff

--- study-2/test.js
+++ study-2/test.js
@@ -1,26 +1,32 @@
 // --- 1. SYNC THE ICON ON LOAD ---
 // This runs as soon as this script file is loaded
 window.addEventListener('DOMContentLoaded', () => {
-    const icon = document.getElementById("theme-icon");
+    const theme_icon = document.getElementById("theme-icon");
+    const logo = document.getElementById("logo");
+
     // Check if the HTML tag has the dark-mode class
     if (document.documentElement.classList.contains("dark-mode")) {
-        if (icon) icon.src = "images/white-moon.png";
+        if (theme_icon) theme_icon.src = "images/white-moon.png";
+        if (logo) logo.src = "images/avatar-dark.png";
     }
 });
 
 // --- 2. THE TOGGLE FUNCTION ---
 function changeTheme() {
     const html = document.documentElement; // Target the HTML tag
-    const icon = document.getElementById("theme-icon");
+    const theme_icon = document.getElementById("theme-icon");
+    const logo = document.getElementById("logo");
 
     // Toggle the class on the HTML tag
     html.classList.toggle("dark-mode");
 
     if (html.classList.contains("dark-mode")) {
-        icon.src = "images/white-moon.png";
+        theme_icon.src = "images/white-moon.png";
+        logo.src = "images/avatar-dark.png";
         localStorage.setItem("theme", "dark");
     } else {
-        icon.src = "images/black-sun.png";
+        theme_icon.src = "images/black-sun.png";
+        logo.src = "images/avatar-light.png";
         localStorage.setItem("theme", "light");
     }
 }
